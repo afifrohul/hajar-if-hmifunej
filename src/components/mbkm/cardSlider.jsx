17764a9ecdc6d0ddab7b44eb702937e0c3dea864
afifrohul <afifmemyself22@gmail.com>
2023-09-17 21:21:36 +0700

    fix(mbkm): card adjust

--- src/components/mbkm/cardSlider.jsx
+++ src/components/mbkm/cardSlider.jsx
@@ -43,6 +43,17 @@ export default function CardTopMbkm(props) {
         return input.replace(/<[^>]*>/g, '')
     }
 
+    const MAX_DESC_LENGTH = 100; // Ganti dengan jumlah karakter maksimum yang Anda inginkan
+    const MAX_PROG = 15; // Ganti dengan jumlah karakter maksimum yang Anda inginkan
+
+    const truncateText = (text, maxLength) => {
+        if (text.length > maxLength) {
+            return text.slice(0, maxLength) + '...';
+        }
+        return text;
+    };
+
+
     return (
         <Swiper
             spaceBetween={50}
@@ -58,15 +69,15 @@ export default function CardTopMbkm(props) {
                 data.map((value, index) => (
                     <SwiperSlide className='px-4'>
                         <div className="shadow-card-mobile lg:shadow-card w-full my-10 rounded-xl lg:rounded-[50px] p-4 lg:p-12 h-full bg-white">
-                            <div>
+                            <div className='h-[6opx] lg:h-[80px]'>
                                 <h3 className='text-xl font-bold lg:text-5xl'>{value.nama_mahasiswa}</h3>
                                 <p className='text-[#B4B6BA] font-bold text-lg lg:text-2xl'>MBKM Batch {value.batch}</p>
                             </div>
-                            <div className='py-4'>
-                                <h5 className='text-[#060919] opacity-70 font-bold text-lg lg:text-2xl'>{value.nama_jenis_mbkm}</h5>
-                                <h5 className='text-[#060919] opacity-70 font-bold text-lg lg:text-2x'>{value.nama_mitra} - {value.nama_program_mbkm}</h5>
-                                <p className='pt-2 text-sm lg:text-xl '>{removeHtmlTags(value.deskripsi_mitra)}</p>
+                            <div className='py-4 h-[80px] lg:h-[120px]'>
+                                <h5 className='text-[#060919] opacity-70 font-bold text-sm lg:text-2xl'>{value.nama_jenis_mbkm}</h5>
+                                <h5 className='text-[#060919] opacity-70 font-bold text-sm lg:text-2xl'>{value.nama_mitra} - {truncateText(value.nama_program_mbkm, MAX_PROG)}</h5>
                             </div>
+                            <p className='pt-2 text-sm lg:text-xl h-[100px]'>{truncateText(removeHtmlTags(value.testimoni_mbkm), MAX_DESC_LENGTH)}</p>
                             <div className='flex justify-between'>
                                 <Link href={value.linkedin} target='_blank' className='flex items-center gap-2 mbkm-linkedin'>
                                     <FaLinkedin className='w-8 h-8 text-[#0077b5]' />

9ee29c75853309f64f431fffc5b6aa367b981a2c
Faisal Bahari <fbhr1112@gmail.com>
2023-09-17 10:57:38 +0700

    fix hero section and search button

--- src/app/mbkm/[jenis]/page.jsx
+++ src/app/mbkm/[jenis]/page.jsx
@@ -13,7 +13,7 @@ export default async function MbkmBidang({ params }) {
                     <>
                         <div className='flex items-center justify-start h-[400px] xl:h-[500px] bg-no-repeat bg-cover bg-primary bg-grid'>
                             <div className='container mt-20 text-white'>
-                                <h1 className='max-w-7xl mb-6 lg:mb-12 text-4xl font-bold lg:text-[100px] lg:leading-[100px]'>Testimoni {content[0].nama_jenis_mbkm}</h1>
+                                <h1 className='max-w-7xl mb-6 lg:mb-12 text-4xl font-bold lg:text-7xl xl:text-[100px] xl:leading-[100px]'>Testimoni {content[0].nama_jenis_mbkm}</h1>
                                 <p className="max-w-4xl text-lg font-medium opacity-80">Bingung mau pilih {content[0].nama_jenis_mbkm} dimana? Nih testimoni {content[0].nama_jenis_mbkm} dari mahasiswa informatika. Simak informasinya ya!</p>
                             </div>
                         </div>
